5ce30344b5df16309c00960b40cfcb275472495b
Divy patel <144588544+Divyp842@users.noreply.github.com>
2025-07-06 23:17:23 +0530

    Update settings.py

--- fitness_tracker/settings.py
+++ fitness_tracker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-82k9x=uppzwe#(b0y-x1sthk-y=q5pn@(0+-adgtyq=jo80p$4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
